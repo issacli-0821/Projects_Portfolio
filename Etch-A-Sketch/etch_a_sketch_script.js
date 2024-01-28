9bc83e3b7b290d1cb00a018b7194eed932eeb3ea
issacli-0821 <tl1719@princeton.edu>
2024-01-27 17:47:19 -0800

    Added logic associated with each button

--- Etch-A-Sketch/etch_a_sketch_script.js
+++ Etch-A-Sketch/etch_a_sketch_script.js
@@ -1,24 +1,55 @@
 const grid = document.querySelector("#grid-container");
 const GRIDSIZE = 600;
 
-const resetButton = document.querySelector("#reset")
+const resetButton = document.querySelector("#reset");
 resetButton.addEventListener("click", resetGrid);
 
-function generateSquare(squaresPerSide)
+let RANDOMIZED_COLOR = false;
+let ERASER = false;
+let STANDARD_COLOR = true;
+
+const standardButton = document.querySelector("#standard");
+standardButton.addEventListener("click", () => {
+    RANDOMIZED_COLOR = false; 
+    ERASER = false;
+    STANDARD_COLOR = true;
+});
+
+const randomButton = document.querySelector("#random");
+randomButton.addEventListener("click", () => {
+    RANDOMIZED_COLOR = true; 
+    ERASER = false;
+    STANDARD_COLOR = false;
+});
+
+const eraserButton = document.querySelector("#eraser");
+eraserButton.addEventListener("click", () => {
+    RANDOMIZED_COLOR = false; 
+    ERASER = true;
+    STANDARD_COLOR = false;
+});
+
+/* Adds a square of size squareSize by squareSize to the main grid */
+function generateSquare(squareSize)
 {
     const square = document.createElement("div");
-    square.style.height = `${GRIDSIZE / squaresPerSide}px`;
-    square.style.width = `${GRIDSIZE / squaresPerSide}px`;
+    square.style.height = squareSize;
+    square.style.width = squareSize;
     square.classList.add("grid-square");
-    square.addEventListener("mouseover", () => square.style.backgroundColor = "blue");
+    square.addEventListener("mouseover", () => square.style.backgroundColor = getColor());
     grid.appendChild(square);
 }
 
+/* Generates squaresPerSide^2 squares for the main grid. Returns nothing */
 function createSquares(squaresPerSide)
 {
+    let squareSize = `${GRIDSIZE / squaresPerSide}px`;
     for (let i = 0; i < squaresPerSide; i++)
     {
-        for (let j = 0; j < squaresPerSide; j++) generateSquare(squaresPerSide);
+        for (let j = 0; j < squaresPerSide; j++) 
+        {
+            generateSquare(squareSize);
+        }
     }
 }
 
@@ -27,6 +58,18 @@ function resetGrid()
     let squaresPerSide = Math.min(parseInt(prompt("Enter the size of your new grid")), 100);
     while (grid.hasChildNodes()) grid.removeChild(grid.firstChild);
     createSquares(squaresPerSide);
+}
+
+function getColor()
+{
+    if (RANDOMIZED_COLOR) return "#" + getRandomRGBValue() + getRandomRGBValue() + getRandomRGBValue();
+    if (ERASER) return "cornflowerblue";
+    return "blue";
+}
+
+function getRandomRGBValue()
+{
+    return Math.floor(Math.random() * 255).toString(16);
 }
 
 createSquares(16);
